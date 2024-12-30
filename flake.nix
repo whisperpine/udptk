9967ccd9e4c520f84166ed6536b6e061257fbad5
Yusong <yusong.lai@icloud.com>
2024-12-30 12:40:47 +0000

    build: add nix develop environment

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                rust-overlay.overlays.default
+                self.overlays.default
+              ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        rustToolchain =
+          let
+            rust = prev.rust-bin;
+          in
+          # rust.stable.latest.default.override {
+          #   extensions = [ "rust-src" ];
+          #   targets = [ ];
+          # };
+          rust.nightly."2024-11-13".default.override {
+            extensions = [ "rust-src" ];
+            targets = [ ];
+          };
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell { packages = with pkgs; [ rustToolchain ]; };
+        }
+      );
+    };
+}
