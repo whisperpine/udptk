09730cd0004a3f927d24408ac26827eba34430d2
Yusong <yusonglai64@gmail.com>
2025-12-29 07:53:26 +0800

    chore: add syntax annotation for the indented string of shellHook

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
               just # just a command runner
             ];
             # The shell script executed when the environment is activated.
-            shellHook = ''
+            shellHook = /* sh */ ''
               # Print the last modified date of "flake.lock".
               git log -1 --format="%cd" --date=format:"%Y-%m-%d" -- flake.lock |
                 awk '{printf "\"flake.lock\" last modified on: %s", $1}' &&
